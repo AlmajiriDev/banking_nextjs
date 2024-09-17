179764a87abfb77ccc1c39904d666aef385352ec
Awwal Adewuyi <almajiri.dev@gmail.com>
2024-09-18 00:08:06 +0100

    implement complete layout

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,13 +1,27 @@
+import MobileNav from '@/components/MobileNav';
+import SideBar from '@/components/SideBar';
+import Image from 'next/image';
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Awwal', lastName: 'Adewuyi' };
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <SideBar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={38} height={38} alt="logo" />{' '}
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,28 @@
+import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react';
 
 const Home = () => {
-  return <div>Home page</div>;
+  const loggedIn = { firstName: 'Awwal', lastName: 'Adewuyi', email: 'almajiri.dev@gmailcom' };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="Home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage you account and transactions efficiently."
+          />
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance: 114.50}, {currentBalance: 245.10}]} />
+    </section>
+  );
 };
 
 export default Home;
